4cd647b398fe70649255340a4bb9f98b18e3d6ea
Li DongYuan(焦点) <dongyuanli217597@sohu-inc.com>
2019-09-03 18:25:33 +0800

    手写jdk动态代理

---
+++ src/main/java/com/example/learn/designPattern/b1Proxy/No3/Person.java
@@ -0,0 +1,8 @@
+package com.example.learn.designPattern.b1Proxy.No3;
+
+public interface Person {
+    // 相亲
+    void findLove();
+    String getSex();
+    String getName();
+}
---
+++ src/main/java/com/example/learn/designPattern/b1Proxy/No3/XiaoXingXing.java
@@ -0,0 +1,32 @@
+package com.example.learn.designPattern.b1Proxy.No3;
+
+public class XiaoXingXing implements Person {
+    private String sex = "女";
+    private String name = "小星星";
+
+    @Override
+    public void findLove() {
+        System.out.println("-----find love----");
+        System.out.println("我叫" + name + "性别是" + sex);
+        System.out.println("我有balabala的择偶标准");
+        System.out.println("-----find love----");
+    }
+
+    @Override
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
