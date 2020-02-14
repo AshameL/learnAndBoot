9e5a49d8823acad134e755cf9fb8389e2dbff359
Li DongYuan(焦点) <dongyuanli217597@sohu-inc.com>
2020-02-14 14:25:36 +0800

    a

--- src/main/java/com/example/noWeb/alg/jianzhiOffer/T64矩阵中的路径.java
+++ src/main/java/com/example/noWeb/alg/jianzhiOffer/T64矩阵中的路径.java
@@ -6,40 +6,48 @@ import java.util.ArrayList;
 
 public class T64矩阵中的路径 {
     public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
-        if(matrix==null || matrix.length==0 || str==null || str.length==0 || matrix.length!=rows*cols || rows<=0 || cols<=0 || rows*cols < str.length) {
-            return false ;
+        if (matrix == null || matrix.length == 0 || str == null || str.length == 0 || matrix.length != rows * cols || rows <= 0 || cols <= 0 || rows * cols < str.length) {
+            return false;
         }
 
-        boolean[] visited = new boolean[rows*cols] ;
-        int[] pathLength = {0} ;
+        boolean[] visited = new boolean[rows * cols];
+        int pathLength =0;
 
-        for(int i=0 ; i<=rows-1 ; i++) {
-            for(int j=0 ; j<=cols-1 ; j++) {
-                if(hasPathCore(matrix, rows, cols, str, i, j, visited, pathLength)) { return true ; }
+        for (int i = 0; i <= rows - 1; i++) {
+            for (int j = 0; j <= cols - 1; j++) {
+                if (hasPathCore(matrix, rows, cols, str, i, j, visited, pathLength)) {
+                    return true;
+                }
             }
         }
 
-        return false ;
+        return false;
     }
 
-    public boolean hasPathCore(char[] matrix, int rows, int cols, char[] str, int row, int col, boolean[] visited, int[] pathLength) {
-        boolean flag = false ;
+    public boolean hasPathCore(char[] matrix, int rows, int cols, char[] str, int row, int col,
+                               boolean[] visited, int pathLength) {
+        boolean flag = false;
 
-        if(row>=0 && row<rows && col>=0 && col<cols && !visited[row*cols+col] && matrix[row*cols+col]==str[pathLength[0]]) {
-            pathLength[0]++ ;
-            visited[row*cols+col] = true ;
-            if(pathLength[0]==str.length) { return true ; }
-            flag = hasPathCore(matrix, rows, cols, str, row, col+1, visited, pathLength)  ||
-                    hasPathCore(matrix, rows, cols, str, row+1, col, visited, pathLength)  ||
-                    hasPathCore(matrix, rows, cols, str, row, col-1, visited, pathLength)  ||
-                    hasPathCore(matrix, rows, cols, str, row-1, col, visited, pathLength) ;
+        if (row >= 0 && row < rows
+                && col >= 0 && col < cols
+                && !visited[row * cols + col]
+                && matrix[row * cols + col] == str[pathLength]) {
+            pathLength++;
+            visited[row * cols + col] = true;
+            if (pathLength == str.length) {
+                return true;
+            }
+            flag = hasPathCore(matrix, rows, cols, str, row, col + 1, visited, pathLength) ||
+                    hasPathCore(matrix, rows, cols, str, row + 1, col, visited, pathLength) ||
+                    hasPathCore(matrix, rows, cols, str, row, col - 1, visited, pathLength) ||
+                    hasPathCore(matrix, rows, cols, str, row - 1, col, visited, pathLength);
 
-            if(!flag) {
-                pathLength[0]-- ;
-                visited[row*cols+col] = false ;
+            if (!flag) {
+                pathLength--;
+                visited[row * cols + col] = false;
             }
         }
 
-        return flag ;
+        return flag;
     }
 }
